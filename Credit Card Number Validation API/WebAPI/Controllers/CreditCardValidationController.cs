639dd3d8b37fe02b42b4ac66d356965033de6c72
Wasenshi <Recka123@gmail.com>
2018-07-17 20:13:04 +0700

    Initialized the projects solution

---
+++ Credit Card Number Validation API/WebAPI/Controllers/CreditCardValidationController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace WebAPI.Controllers
+{
+    public class CreditCardValidationController : ApiController
+    {
+        public IHttpActionResult Index()
+        {
+
+            return this.Ok();
+        }
+    }
+}
